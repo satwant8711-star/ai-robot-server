2769c1ca0679170f6d6acd6a33d440fb0d766ad4
Satwant Singh <satwant_23yahoo.com>
2026-03-10 02:44:37 +0530

    fix wav conversion for esp32 audio

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import requests
 import os
 from gtts import gTTS
 import tempfile
+import wave
 
 app = FastAPI()
 
@@ -17,32 +18,37 @@ CHAT_URL = "https://api.groq.com/openai/v1/chat/completions"
 async def robot(file: UploadFile = File(...)):
 
     try:
-        # -------- Read uploaded audio --------
-        audio_bytes = await file.read()
 
-        # Save temporarily
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
-            temp_audio.write(audio_bytes)
-            temp_path = temp_audio.name
+        # -------- read uploaded raw audio --------
+        raw_audio = await file.read()
+
+        # -------- create proper WAV file --------
+        temp_wav = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
+
+        with wave.open(temp_wav.name, 'wb') as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)   # 16-bit audio
+            wf.setframerate(16000)
+            wf.writeframes(raw_audio)
 
         # -------- Speech to Text --------
         headers = {
             "Authorization": f"Bearer {GROQ_API_KEY}"
         }
 
-        stt_files = {
-            "file": ("audio.wav", open(temp_path, "rb"), "audio/wav")
+        files = {
+            "file": ("audio.wav", open(temp_wav.name, "rb"), "audio/wav")
         }
 
-        stt_data = {
+        data = {
             "model": "whisper-large-v3"
         }
 
         stt_response = requests.post(
             STT_URL,
             headers=headers,
-            files=stt_files,
-            data=stt_data
+            files=files,
+            data=data
         )
 
         stt_json = stt_response.json()
